b5a5e981aa48e51a1f00399865aa547c58312e6b
Bayero-abdul <bayeroa65@gmail.com>
2022-10-30 12:25:22 +0000

    Update code

--- console.py
+++ console.py
@@ -7,8 +7,10 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """HBNB command interpreter"""
 
+    prompt = '(hbnb) '
+
     def do_EOF(self, arg):
-        """end of file i.e ctrl+d"""
+        """EOF(end of file) i.e Ctrl+D"""
         try:
             input()
         except EOFError:
@@ -17,18 +19,6 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program"""
         exit()
-
-    def create:
-        if ARG(CLASSNAME)
-            create a BASEMODEL INSTANCE
-            SAVE it to the json file
-            print ID
-        else if If the class name is missing:
-            print ** class name missing **
-        else if class name does not exist:
-            print ** class doesn't exist **
-
-
 
     def emptyline(self):
         """Ignores when empty line or ENTER is being pressed"""
