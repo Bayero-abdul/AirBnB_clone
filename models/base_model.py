a0e37e75d6c6ef973af06c269bc718a69aa9d82d
Bayero-abdul <bayeroa65@gmail.com>
2022-11-02 07:53:52 +0000

    Reworking Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,23 @@ from datetime import datetime
 class BaseModel:
     """defines all commom attributes/methods for other classes."""
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Instantiates instances of an object."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            self.set_attributes(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+    
+    def set_attributes(self, kwargs):
+        """set obj attributes from key/values."""
+        for key, value in kwargs.items():
+            if key != '__class__':
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+
 
     def __str__(self):
         """string representaion of the instances."""
