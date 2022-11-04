590e1cb26468fbaed423680c1aed6624e73bc6ab
Bayero-abdul <bayeroa65@gmail.com>
2022-11-04 15:39:24 +0000

    Update code

--- models/base_model.py
+++ models/base_model.py
@@ -14,29 +14,21 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instantiates instances of an object."""
         if kwargs:
-            self.set_attributes(kwargs)
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
 
-    def set_attributes(self, kwargs):
-        """set obj attributes from key/values."""
-        for key, value in kwargs.items():
-            if key != '__class__':
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-
     def __str__(self):
         """string representaion of the instances."""
-        copy = self.__dict__
-        if type(copy['created_at']) is str:
-            copy['created_at'] = datetime.fromisoformat(copy['created_at'])
-        if type(copy['updated_at']) is str:
-            copy['updated_at'] = datetime.fromisoformat(copy['updated_at'])
-        return ("[BaseModel] ({}) {}".format(self.id, copy))
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id,
+                                      self.__dict__))
 
     def save(self):
         """Updates the updated_at date."""
@@ -46,8 +38,11 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values of \
          __dict__ of the instance."""
-        to_dict = self.__dict__
+        from copy import deepcopy
+        to_dict = deepcopy(self.__dict__)
         to_dict['__class__'] = self.__class__.__name__
-        to_dict['created_at'] = self.created_at.isoformat()
-        to_dict['updated_at'] = self.updated_at.isoformat()
+        for key in self.__dict__:
+            if key in ("created_at", "updated_at"):
+                value = self.__dict__[key].isoformat()
+                to_dict[key] = value
         return to_dict
