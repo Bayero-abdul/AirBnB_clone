afd73750b3512f19396454b0957daeec8cb9fef5
Bayero-abdul <bayeroa65@gmail.com>
2022-11-08 20:07:16 +0000

    Update class variables

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,26 +16,26 @@ JSON file to instances."""
 
     def all(self):
         """returns the dictionary __objects"""
-        return self.__objects
+        return type(self).__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         class_name = obj.__class__.__name__
         key = class_name + "." + obj.id
-        self.__objects[key] = obj
+        type(self).__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
         with open(self.__file_path, "w", encoding="utf-8") as f:
-            for key, value in self.__objects.items():
-                self.__objects[key] = value.to_dict()
-            f.write(json.dumps(self.__objects))
+            for key, value in type(self).__objects.items():
+                type(self).__objects[key] = value.to_dict()
+            f.write(json.dumps(type(self).__objects))
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         from models.base_model import BaseModel
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as f:
+            with open(type(self).__file_path, "r", encoding="utf-8") as f:
                 read = json.loads(f.read())
                 for key, value in read.items():
-                    self.__objects[key] = BaseModel(**value)
+                    type(self).__objects[key] = BaseModel(**value)
