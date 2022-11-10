9dc36997fff42d644281385a890b302e804d90af
Bayero-abdul <bayeroa65@gmail.com>
2022-11-10 19:57:15 +0000

    Import user in console.py

--- console.py
+++ console.py
@@ -6,11 +6,11 @@
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
 from models.engine.file_storage import FileStorage
 
 
-hbnb_classes = {'BaseModel': BaseModel,
-                'User': User}
+hbnb_classes = {'BaseModel': BaseModel, 'User': User}
 
 
 class HBNBCommand(cmd.Cmd):
