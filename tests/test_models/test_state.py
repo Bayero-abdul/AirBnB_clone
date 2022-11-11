3d606ba89f25749ae81c7dec4a770f411bcb9c26
Bayero-abdul <bayeroa65@gmail.com>
2022-11-11 12:01:54 +0000

    Test for the State class

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Test module for the `State class`.
+"""
+
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """TestCase for the `State class`."""
+
+    def setUp(self):
+        """setup code."""
+        self.my_state = State()
+
+    def test_instance(self):
+        """test for instances."""
+        self.assertTrue(isinstance(self.my_state, State))
+    
+    def test_issubclass_BaseModel(self):
+        """tests if State is a subclass of BaseModel."""
+        self.assertTrue(issubclass(type(self.my_state), BaseModel))
+
+    def test_name_attr(self):
+        """test for public attribute `name`."""
+        self.assertTrue(hasattr(self.my_state, 'name'))
+        self.assertTrue(self.my_state.name == '')
